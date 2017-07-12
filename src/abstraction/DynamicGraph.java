6aae58f8e9c8a4100eebb28570368fae525066ff
unknown <Aksenov239@AKSENOV.zoo.local>
2017-07-12 20:33:20 +0400

    first non-working implementation of concurrent graph

---
+++ src/abstraction/DynamicGraph.java
@@ -0,0 +1,12 @@
+package abstraction;
+
+/**
+ * User: Aksenov Vitaly
+ * Date: 12.07.2017
+ * Time: 14:35
+ */
+public interface DynamicGraph {
+    public boolean isConnected(int u, int v);
+    public void addEdge(int u, int v);
+    public void removeEdge(int u, int v);
+}
---
+++ src/test/Test.java
@@ -0,0 +1,43 @@
+package test;
+
+import sequential.SequentialDynamicGraph;
+
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+/**
+ * User: Aksenov Vitaly
+ * Date: 12.07.2017
+ * Time: 17:16
+ */
+public class Test {
+    public static void main(String[] args) {
+        new Test().run();
+    }
+
+    public void run() {
+        Scanner in = new Scanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+
+        int n = in.nextInt();
+        int q = in.nextInt();
+
+        SequentialDynamicGraph sdg = new SequentialDynamicGraph(n);
+
+        for (int i = 0; i < q; i++) {
+            String query = in.next();
+            int u = in.nextInt() - 1;
+            int v = in.nextInt() - 1;
+
+            if (query.equals("?")){
+                out.println(sdg.isConnected(u, v));
+            } else if (query.equals("+")) {
+                sdg.addEdge(u, v);
+            } else {
+                sdg.removeEdge(u, v);
+            }
+        }
+
+        out.close();
+    }
+}
